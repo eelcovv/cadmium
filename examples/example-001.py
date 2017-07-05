3f7c11324b2e219ae4b087d6193b24f4868f601c
Eelco van Vliet <evanvliet@hmc-heerema.com>
2017-07-05 06:32:35 +0200

    working on cadmium import but we need pythonocc-core 0.18 whereas we need 0.17 for free cad. This is a no-go

--- examples/example-001.py
+++ examples/example-001.py
@@ -9,15 +9,16 @@ try:
     stlfname = sys.argv[1]
 except IndexError:
     from os.path import basename, splitext
+
     stlfname = splitext(basename(__file__))[0] + ".stl"
 
 c0 = Cylinder(radius=1, height=8, center=True)
 c1 = Cylinder(radius=1, height=8, center=True).rotate(X_axis, 45)
 c2 = Cylinder(radius=1, height=8, center=True).rotate(X_axis, -45)
 c3 = Cylinder(radius=1, height=8, center=True).rotate(X_axis, 90)
-b = Box(x=8,y=1,z=1,center=True)
-s0 = Sphere(r=1).translate(1.5,0,0)
-s1 = Sphere(r=1).translate(-1.5,0,0)
+b = Box(x=8, y=1, z=1, center=True)
+s0 = Sphere(r=1).translate(1.5, 0, 0)
+s1 = Sphere(r=1).translate(-1.5, 0, 0)
 
 u = ((c0 + c1 + c2 + c3) - b) - (s0 + s1)
 
--- src/cadmium/__init__.py
+++ src/cadmium/__init__.py
@@ -1,24 +1,27 @@
 '''
 Python library for Solid Modelling
 '''
+from __future__ import absolute_import
 
+from builtins import str
 import os
 
-from OCC.gp import *
-X_axis = gp_Ax1(gp_Pnt(0,0,0),gp_Dir(1,0,0))
-Y_axis = gp_Ax1(gp_Pnt(0,0,0),gp_Dir(0,1,0))
-Z_axis = gp_Ax1(gp_Pnt(0,0,0),gp_Dir(0,0,1))
+from OCC.gp import (gp_Ax1, gp_Pnt, gp_Dir)
 
-import solid 
-import primitives.cylinder
-import primitives.sphere
-import primitives.box
-import primitives.cone
-import primitives.wedge
-import primitives.torus
-import primitives.text
-import primitives.revolution
-import primitives.extrusion
+from . import solid
+from .primitives import cylinder
+from .primitives import sphere
+from .primitives import box
+from .primitives import cone
+from .primitives import wedge
+from .primitives import torus
+from .primitives import text
+from .primitives import revolution
+from .primitives import extrusion
+
+X_axis = gp_Ax1(gp_Pnt(0, 0, 0), gp_Dir(1, 0, 0))
+Y_axis = gp_Ax1(gp_Pnt(0, 0, 0), gp_Dir(0, 1, 0))
+Z_axis = gp_Ax1(gp_Pnt(0, 0, 0), gp_Dir(0, 0, 1))
 
 Cylinder = primitives.cylinder.Cylinder
 Sphere = primitives.sphere.Sphere
@@ -34,8 +37,8 @@ Extrusion = primitives.extrusion.Extrusion
 Solid = solid.Solid
 
 inspectionData = {
-  'solidData' : {},
-  'paramData' : {}
+    'solidData': {},
+    'paramData': {}
 }
 
 # Value range constants
@@ -51,76 +54,83 @@ A_CENTER = 3
 enum = 1
 
 try:
-    _brep_cache_path_ = os.path.join(os.environ['HOME'],'.cadmium','brepcache')
+    _brep_cache_path_ = os.path.join(os.environ['HOME'], '.cadmium', 'brepcache')
 except KeyError:
-  _brep_cache_path_ = os.path.join(os.environ['USERPROFILE'],'.cadmium','brepcache')
+    _brep_cache_path_ = os.path.join(os.environ['USERPROFILE'], '.cadmium', 'brepcache')
 _brep_caching_enabled_ = True
 try:
-    _font_dir_ = os.path.join(os.environ['HOME'],'.cadmium','fonts')
+    _font_dir_ = os.path.join(os.environ['HOME'], '.cadmium', 'fonts')
 except KeyError:
-  _font_dir_ = os.path.join(os.environ['USERPROFILE'],'.cadmium','fonts')
+    _font_dir_ = os.path.join(os.environ['USERPROFILE'], '.cadmium', 'fonts')
 _abs_fontpath_allowed_ = True
 
 if not os.path.exists(_brep_cache_path_):
-  os.makedirs(_brep_cache_path_)
+    os.makedirs(_brep_cache_path_)
+
 
 #
 # Annotation decorators
 #
 def description(*arg, **kwdArg):
+    validArgs = ['shortName', 'summary', 'alignment']
+    for k in list(kwdArg.keys()):
+        if not (k in validArgs):
+            raise Exception('Invalid argument ' + str(k))
 
-  validArgs = ['shortName','summary', 'alignment']
-  for k in kwdArg.keys():
-    if not(k in validArgs):
-      raise Exception('Invalid argument '+str(k))
+    if arg:
+        raise Exception('Only named arguments supported')
 
-  if arg:
-    raise Exception('Only named arguments supported')
+    import cadmium
+    cadmium.inspectionData['solidData'] = kwdArg
 
-  import cadmium
-  cadmium.inspectionData['solidData'] = kwdArg
+    def decorator(func):
+        return func
 
-  def decorator(func): return func
-  return decorator
+    return decorator
+
 
 def param(*arg, **kwdArg):
+    validArgs = ['name', 'shortName', 'description',
+                 'valueRange', 'valueType', 'invalidValues', 'validValues', 'endpointInclusion']
+    for k in list(kwdArg.keys()):
+        if not (k in validArgs):
+            raise Exception('Invalid argument ' + str(k))
 
-  validArgs = ['name','shortName','description',
-    'valueRange','valueType','invalidValues','validValues','endpointInclusion']
-  for k in kwdArg.keys():
-    if not(k in validArgs):
-      raise Exception('Invalid argument '+str(k))
+    if arg:
+        raise Exception('Only named arguments supported')
 
-  if arg:
-    raise Exception('Only named arguments supported')
+    import cadmium
+    if 'valueType' in kwdArg:
+        if kwdArg['valueType'] == int:
+            kwdArg['valueType'] = 'int'
+        elif kwdArg['valueType'] == float:
+            kwdArg['valueType'] = 'float'
+        elif kwdArg['valueType'] == str:
+            kwdArg['valueType'] = 'str'
+        elif kwdArg['valueType'] == bool:
+            kwdArg['valueType'] = 'bool'
+        elif kwdArg['valueType'] == cadmium.enum:
+            kwdArg['valueType'] = 'enum'
+        else:
+            kwdArg['valueType'] = 'unknown'
 
-  import cadmium
-  if kwdArg.has_key('valueType'):
-    if kwdArg['valueType'] == int:
-      kwdArg['valueType'] = 'int'
-    elif kwdArg['valueType'] == float:
-      kwdArg['valueType'] = 'float'
-    elif kwdArg['valueType'] == str:
-      kwdArg['valueType'] = 'str'
-    elif kwdArg['valueType'] == bool:
-      kwdArg['valueType'] = 'bool'
-    elif kwdArg['valueType'] == cadmium.enum:
-      kwdArg['valueType'] = 'enum'
-    else:
-      kwdArg['valueType'] = 'unknown'
+    cadmium.inspectionData['paramData'][kwdArg['name']] = kwdArg
 
-  cadmium.inspectionData['paramData'][kwdArg['name']] = kwdArg
+    def decorator(func):
+        return func
 
-  def decorator(func): return func
-  return decorator
+    return decorator
+
 
 class CadmiumException(BaseException):
-  '''
-  Useful mainly for validating user provided input for Solid instantiations
-  '''
-  def __init__(self, msg):
-    self.msg = msg
-    BaseException.__init__(self, msg)
+    '''
+    Useful mainly for validating user provided input for Solid instantiations
+    '''
+
+    def __init__(self, msg):
+        self.msg = msg
+        BaseException.__init__(self, msg)
+
 
 def garbage_collect():
-  GarbageCollector.garbage.smart_purge()
+    GarbageCollector.garbage.smart_purge()
